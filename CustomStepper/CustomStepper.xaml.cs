e6de06a44958202f4e9f2be0070ed99cf666217b
ThePhoenix93 <francescofinotto@hotmail.com>
2020-01-07 17:43:14 +0100

    initial release

---
+++ CustomStepper/CustomStepper.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TPSoftware.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomStepper : Grid
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Value { get; set; }
+
+        public event Action OnValueCanged;
+        public CustomStepper()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPlus_Clicked(object sender, EventArgs e)
+        {
+            Value++;
+        }
+
+        private void ButtonMinus_Clicked(object sender, EventArgs e)
+        {
+            Value--;
+        }
+    }
+}
